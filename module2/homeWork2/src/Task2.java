bec6520d079dce9e0b3250a2d3636681d1645018
Stan <Ess1206@gmail.com>
2016-08-24 20:08:05 +0300

    HomeWork Task2

--- module2/homeWork2/src/Task2.java
+++ module2/homeWork2/src/Task2.java
@@ -2,8 +2,30 @@
  * Created by Stan on 24.08.2016.
  */
 public class Task2 {
+    //HOMEWORK 2.1
+
+    static double commission;
+    static double withdrawBalance(double balance, double withdrawal) {
+      commission = withdrawal/20;
+
+        return balance - withdrawal - commission;
+    }
 
     public static void main(String[] args) {
 
+        double balanceAfter = withdrawBalance(100,99);
+
+
+
+        if(balanceAfter >= 0) {
+            System.out.println("ok");
+            System.out.println(commission);
+            System.out.println(balanceAfter);
+        }
+        else {
+            System.out.println("no");
+        }
     }
+
+
 }
